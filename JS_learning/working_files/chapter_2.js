2e48e2a88294d6b5af1de7181ba03592bef0e2ce
taraschubar <66127899+taraschubar@users.noreply.github.com>
2024-11-28 22:24:49 +0100

    Update chapter_2.js

--- JS_learning/working_files/chapter_2.js
+++ JS_learning/working_files/chapter_2.js
@@ -14,11 +14,42 @@ let is_True = true
 
 function myFirstFunction(value, secondValue) {
 
-console.log(value * secondValue)
+console.log(value != secondValue)
 }
 
 let multipleTwoNumber = myFirstFunction
 
 multipleTwoNumber(3,5)
-multipleTwoNumber(5,7)
+multipleTwoNumber('5',5)
 multipleTwoNumber(2,8)
+
+
+// && - if both conditions are the same 
+// '//' if one consdition of second condition is true
+// ! - gonna inverse the condition
+
+let condition1 = true
+let condition2 = false
+
+console.log(condition1 || condition2)
+
+console.log(typeof example_array)
+
+
+// IF statement 
+
+if  (condition1 && condition2){
+    console.log('hi mom')
+} else if (condition1 || condition2){
+    console.log('one condition of the two was true')
+ 
+}else{
+    console.log('the value was wrong so here we are ')
+}
+
+
+
+
+
+
+
