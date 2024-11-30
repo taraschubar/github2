1076f3f491c7148fb961cb1de3ef6500cd6f140a
taraschubar <66127899+taraschubar@users.noreply.github.com>
2024-11-30 16:26:19 +0100

    Create chapter_3.js

---
+++ JS_learning/working_files/chapter_3.js
@@ -0,0 +1,28 @@
+// for (let i = 0; i <20; i++){
+//     console.log('The value of i is: ', i)
+// }
+
+// let val = 4;
+// if (val < 5){
+//     console.log('Value is smaller than 5')
+// } else{
+//     console.log('Value is bigger than 5')
+// }
+
+
+// let name = 'Taras';
+
+// if (name.includes('r')){
+//     console.log('Name includes r')
+// } else if (name.includes('p')){
+//     console.log('Name includes p')
+// } else {
+//     console.log('name did not contain r or p')
+// }
+
+
+function printName(name){
+    console.log('The persons name is: ', name)
+}
+let firstName = 'Taras'
+printName(firstName)
